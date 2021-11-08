ca522aafc2da491f4abd528100e77b8489796cef
hallo <hallo@LAPTOP-OAFNHH2C>
2021-11-08 14:49:35 -0600

    21.11.05 Se suve el pryecto del modulo 1.

---
+++ src/Proyecto/Modulo1/MongoDBConnection.java
@@ -0,0 +1,21 @@
+package proyecto;
+class MongoDBConnection {
+	
+	private String database;
+
+	public MongoDBConnection(String database)
+	{
+	    this.database = database;
+	}
+	
+	public String getDatabase()
+	{
+	    return database;
+	}
+	
+	public void setDatabase(String database)
+	{
+	    this.database = database;
+	}
+
+}
---
+++ src/Proyecto/Modulo1/MongoDBDatabase.java
@@ -0,0 +1,29 @@
+package proyecto;
+public class MongoDBDatabase {
+	MongoDBConnection connection;
+
+    public void connect(String database) {
+        connection = new MongoDBConnection(database);
+    }
+
+    public void insert(String comando) {
+        System.out.println("Inserting " + comando);
+    }
+
+    public void update(String comando) {
+        System.out.println("Updating " + comando);
+    }
+
+    public void delete(String comando) {
+        System.out.println("Deleting " + comando);
+    }
+    public void consulta (String comando)
+    {
+    	 System.out.println("Select " + comando);
+    }
+    
+    public void disconnect() {
+        connection = null;
+    }
+
+}
